f085a71f9b1bb19b168098efce7343369aa2a684
twmushaurwi <twmushaurwi@gmail.com>
2023-10-26 13:08:59 -0700

    task1 git commit -m task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,15 +3,15 @@
 #include<string.h>
 #include"main.h"
 
-/*
- * binary_to_uint: converts a string of 0's and 1's to an unsigned integer
- * @param b: a pointer to a string of 0's and 1's
- * @return: an unsigned integer, or -1 on error
+/**
+ * binary_to_uint - converts a string to a decimal
+ * @b: a character to a pointer
+ * Return - the converted decimal (output)
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int i;
+	unsigned int decimal = 0;
+	int i;
 
 	if (b == NULL)
 	return (0);
