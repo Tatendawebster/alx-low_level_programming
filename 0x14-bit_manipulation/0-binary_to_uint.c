ca8564ae551b593766c41bd03636bb9659ef5a16
twmushaurwi <twmushaurwi@gmail.com>
2023-10-26 12:21:25 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int decimal = 0;
-    int i;
+int i;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+	return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
+	for (i = 0; b[i] != '\0'; i++)
+{
+	if (b[i] != '0' && b[i] != '1')
+	return (0);
 
-        decimal = decimal * 2 + (b[i] - '0');
-    }
+	decimal = decimal * 2 + (b[i] - '0');
+}
 
-    return (decimal);
+return (decimal);
 }
