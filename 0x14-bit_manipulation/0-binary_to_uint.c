3511708ab729be9f9fd98099ea53a1ffb884d754
twmushaurwi <twmushaurwi@gmail.com>
2023-10-26 12:13:49 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<string.h>
 #include"main.h"
 
@@ -9,23 +10,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned char result = 0;
-	unsigned int i = 0;
+unsigned int decimal = 0;
+    int i;
 
-switch (b[i])
-{
-	case '0':
-	result += 1;
-	break;
-	case '1':
-	result += 2;
-	break;
-	default:
-	result = -1;
-break;
-}
+    if (b == NULL)
+        return (0);
 
-if (i == strlen(b) - 1)
-result = result - 1;
-return (result);
+    for (i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] != '0' && b[i] != '1')
+            return (0);
+
+        decimal = decimal * 2 + (b[i] - '0');
+    }
+
+    return (decimal);
 }
